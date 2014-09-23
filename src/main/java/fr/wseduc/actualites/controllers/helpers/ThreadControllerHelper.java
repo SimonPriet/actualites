6ce07c309e219d632be8b9bebfc5c6be19d336b3
zkourrida <zakaria.kourrida@atos.net>
2014-09-23 18:27:46 +0200

    [Actualites] notify shared categorie

--- src/main/java/fr/wseduc/actualites/controllers/helpers/ThreadControllerHelper.java
+++ src/main/java/fr/wseduc/actualites/controllers/helpers/ThreadControllerHelper.java
@@ -16,6 +16,7 @@ import org.vertx.java.core.Handler;
 import org.vertx.java.core.Vertx;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.http.RouteMatcher;
+import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Container;
 
 import fr.wseduc.actualites.services.ThreadService;
@@ -25,28 +26,30 @@ public class ThreadControllerHelper extends MongoDbControllerHelper {
 
 	protected final String managedCollection;
 	protected final String type;
-	
+
 	protected final ThreadService threadService;
 	protected ShareService shareService;
-	
+
+	private static final String EVENT_TYPE = "NEWS";
+
 	public ThreadControllerHelper(String managedCollection, final ThreadService threadService) {
 		this(managedCollection, threadService, null);
 	}
-	
+
 	public ThreadControllerHelper(String managedCollection, final ThreadService threadService, Map<String, List<String>> groupedActions) {
 		super(managedCollection, groupedActions);
 		this.threadService = threadService;
 		this.managedCollection = managedCollection;
 		this.type = managedCollection.toUpperCase();
 	}
-	
+
 	@Override
 	public void init(Vertx vertx, Container container, RouteMatcher rm, Map<String, SecuredAction> securedActions) {
 		super.init(vertx, container, rm, securedActions);
-	
+
 		this.shareService = new MongoDbShareService(eb, mongo, managedCollection, securedActions, null);
-	}	
-	
+	}
+
 	public void listThreads(final HttpServerRequest request) {
 		UserUtils.getUserInfos(eb, request, new Handler<UserInfos>() {
 			@Override
@@ -67,7 +70,7 @@ public class ThreadControllerHelper extends MongoDbControllerHelper {
 			}
 		});
 	}
-	
+
 	public void retrieveThread(final HttpServerRequest request) {
 		UserUtils.getUserInfos(eb, request, new Handler<UserInfos>() {
 			@Override
@@ -77,27 +80,48 @@ public class ThreadControllerHelper extends MongoDbControllerHelper {
 			}
 		});
 	}
-	
+
 	public void createThread(final HttpServerRequest request) {
 		create(request);
 	}
-	
+
 	public void updateThread(final HttpServerRequest request) {
 		update(request);
 	}
-	
+
 	public void deleteThread(final HttpServerRequest request) {
 		delete(request);
 	}
-	
+
 	public void shareThread(final HttpServerRequest request) {
 		shareJson(request, false);
 	}
-	
+
 	public void shareThreadSubmit(final HttpServerRequest request) {
-		shareJsonSubmit(request, null, false);
+		UserUtils.getUserInfos(eb, request, new Handler<UserInfos>() {
+			@Override
+			public void handle(final UserInfos user) {
+				if (user != null) {
+					final String threadId = request.params().get("id");
+					if(threadId == null || threadId.trim().isEmpty()) {
+			            badRequest(request);
+			            return;
+			        }
+					setTimelineEventType(EVENT_TYPE);
+					JsonObject params = new JsonObject()
+						.putString("profilUri", container.config().getString("host") +
+								"/userbook/annuaire#" + user.getUserId() + "#" + user.getType())
+						.putString("username", user.getUsername())
+						.putString("resourceUri", container.config().getString("host") + pathPrefix +
+								"#/view/thread/" + threadId);
+					shareJsonSubmit(request, "notify-thread-shared.html", false, params, "title");
+				} else {
+					unauthorized(request);
+				}
+			}
+		});
 	}
-	
+
 	public void shareThreadRemove(final HttpServerRequest request) {
 		removeShare(request, false);
 	}
